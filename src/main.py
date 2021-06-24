ffb21fbcf72cf7a4e66feca4446a5282a1628c0a
Umbriona <sandra.viknander@gmail.com>
2021-06-24 23:57:21 +0200

    added main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,33 @@
-from pathlib import Path
-print('Running' if __name__ == '__main__' else 'Importing', Path(__file__).resolve())
-from test import test_utils
+import argparse
+import pandas as pd
+import src.utils as utils
 
-def main():
-    print("HI")
+parser = argparse.ArgumentParser(""" """)
+parser.add_argument('-d', "--connection_data", type=str, required = True)
+parser.add_argument('-p', "--pfam_data", type=str, required = True)
+
+parser.add_argument('-c', "--connection_condition", type = int, default = 500)
+parser.add_argument('-n', "--node_degree", type = int, default = 100 )
+
+parser.add_argument('-o', "--output", type = str, default = "results/boxplot.png")
+
+def main(args):
+    
+    df_high, df_low = utils.prepare_data(args.connection_data, args.connection_condition)
+    node_degree_high, node_degree_low = utils.network_degree(df_high, args.node_degree)
+    
+    df_pfam = pd.read_table(args.pfam_data)
+    
+    df_degree_high = utils.add_pfam2frames(df_pfam, node_degree_high , label = "High")
+    df_degree_low = utils.add_pfam2frames(df_pfam, node_degree_low , label = "Low")
+    
+    df_degree_highlow = pd.concat([df_degree_high, df_degree_low])
+    
+    utils.plot_stat(df_degree_highlow, args.output)
+    
+    
     return 0
 
 if __name__ == "__main__":
-    main()
+    args = parser.parse_args()
+    main(args)
