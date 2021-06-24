752444110e8c972fff890155b7a032ca76db8da0
Umbriona <sandra.viknander@gmail.com>
2021-06-24 23:56:35 +0200

    expanded tests

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,16 +1,23 @@
 import os
 import unittest
+import pandas as pd
 from src import utils
 
+# Define (static variables)
 INTERACTION_FILE = "data/9606.protein.links.v11.0.txt" 
+PFAM_FILE = "data/proteins_w_domains.txt"
+CONDITION_CONECTION = 500
+CONDITION_DEGREE = 100
+
+
 
 class TestUtilsPreparedata(unittest.TestCase):
-    def test_split(self):
-        df_high, df_low = utils.prepare_data(INTERACTION_FILE, 500)
-        
-        self.assertLessEqual(500, min(df_high["combined_score"]))
-        self.assertGreater(500, max(df_low["combined_score"]))
-        self.assertEqual(6, len(df_low.keys()) + len(df_high.keys()))
+    def test_split(self): 
+        DF_HIGH, DF_LOW = utils.prepare_data(INTERACTION_FILE, CONDITION_CONECTION)
+        self.assertLessEqual(CONDITION_CONECTION, min(DF_HIGH["combined_score"]))
+        self.assertGreater(CONDITION_CONECTION, max(DF_LOW["combined_score"]))
+        self.assertEqual(6, len(DF_LOW.keys()) + len(DF_HIGH.keys()))
+
         
     def test_values(self):
         self.assertRaises(ValueError, utils.prepare_data, INTERACTION_FILE, 0)
@@ -21,5 +28,46 @@ class TestUtilsPreparedata(unittest.TestCase):
         self.assertRaises(TypeError, utils.prepare_data, INTERACTION_FILE, True)
         self.assertRaises(TypeError, utils.prepare_data, INTERACTION_FILE, "10")
         
+class TestUtilsMakeNetworks(unittest.TestCase):
+
+        
+    def test_type(self):
+        df_empty = pd.DataFrame({'A' : []})
+        
+        self.assertRaises(TypeError, utils.network_degree, 10, CONDITION_DEGREE)
+        self.assertRaises(TypeError, utils.network_degree, True, CONDITION_DEGREE)
+        self.assertRaises(TypeError, utils.network_degree, "10", CONDITION_DEGREE)
+        self.assertRaises(TypeError, utils.network_degree, df_empty, 10+2j)
+        self.assertRaises(TypeError, utils.network_degree, df_empty, "10")
+        self.assertRaises(TypeError, utils.network_degree, df_empty, False)
+        
+    def test_values(self): 
+        DF_HIGH, DF_LOW = utils.prepare_data(INTERACTION_FILE, CONDITION_CONECTION)
+        DF_DEGREE_HIGH, DF_DEGREE_LOW = utils.network_degree(DF_HIGH, CONDITION_DEGREE)
+        
+        self.assertEqual(4, len(DF_DEGREE_HIGH.keys()) + len(DF_DEGREE_LOW.keys()))
+        self.assertFalse(bool(set(DF_DEGREE_HIGH["Protein"]) & set(DF_DEGREE_LOW["Protein"])))
+        
+class TestUtilsPfam(unittest.TestCase):
+
+        
+    def test_type(self):
+        
+        df_empty = pd.DataFrame({'A' : []})
+        self.assertRaises(TypeError, utils.add_pfam2frames, 10, df_empty)
+        self.assertRaises(TypeError, utils.add_pfam2frames, True, df_empty)
+        self.assertRaises(TypeError, utils.add_pfam2frames, "10", df_empty)
+        self.assertRaises(TypeError, utils.add_pfam2frames, df_empty, 10+2j)
+        self.assertRaises(TypeError, utils.add_pfam2frames, df_empty, "10")
+        self.assertRaises(TypeError, utils.add_pfam2frames, df_empty, False)
+        
+    def test_shape(self):
+        DF_HIGH, DF_LOW = utils.prepare_data(INTERACTION_FILE, CONDITION_CONECTION)
+        DF_DEGREE_HIGH, DF_DEGREE_LOW = utils.network_degree(DF_HIGH, CONDITION_DEGREE)
+        DF_PFAM = pd.read_table(PFAM_FILE)
+        DF_DEGREE_PFAM = utils.add_pfam2frames(DF_PFAM, DF_DEGREE_HIGH)
+        
+        self.assertEqual(4, len(DF_DEGREE_PFAM.keys()))
+        self.assertFalse(any(list(DF_DEGREE_PFAM.isna()["Pfam IDs"])))
         
         
