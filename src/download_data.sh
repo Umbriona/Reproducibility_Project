eef0e82cba4ab71d3a7668bddb4488c9a32517dc
Umbriona <33811221+Umbriona@users.noreply.github.com>
2021-06-29 11:51:47 +0200

    change download links

--- src/download_data.sh
+++ src/download_data.sh
@@ -1,9 +1,12 @@
 DIR=data/
-SITE=https://stringdb-static.org/download/protein.links.v11.0/9606.protein.links.v11.0.txt.gz
-FILE=9606.protein.links.v11.0.txt.gz
+SITE_INTERACTIONS=https://stringdb-static.org/download/protein.links.v11.0/9606.protein.links.v11.0.txt.gz
+SiTE_PFAM=https://chalmersuniversity.box.com/shared/static/08tmtk9adq43f9af9z7b91i0bnpqhd15.txt
+
+FILE_INTERACTIONS=protein_links.txt.gz
+FILE_PFAM=protein_domains.txt
 
 cd $DIR
-wget $SITE
-gunzip $FILE
-#rm $FILE
+wget $SITE_INTERACTIONS -O $FILE_INTERACTIONS
+gunzip $FILE_INTERACTIONS
+wget $SITE_PFAM -O $FILE_PFAM
 cd ..
