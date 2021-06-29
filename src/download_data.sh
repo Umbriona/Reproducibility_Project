4d8994f61a0f0864e01a863a0a51db5f9bac8f64
Umbriona <33811221+Umbriona@users.noreply.github.com>
2021-06-29 12:29:04 +0200

    add download of protein domains

--- src/download_data.sh
+++ src/download_data.sh
@@ -1,12 +1,13 @@
 DIR=data/
 SITE_INTERACTIONS=https://stringdb-static.org/download/protein.links.v11.0/9606.protein.links.v11.0.txt.gz
-SiTE_PFAM=https://chalmersuniversity.box.com/shared/static/08tmtk9adq43f9af9z7b91i0bnpqhd15.txt
+SITE_PFAM=https://chalmersuniversity.box.com/shared/static/08tmtk9adq43f9af9z7b91i0bnpqhd15.txt
 
 FILE_INTERACTIONS=protein_links.txt.gz
 FILE_PFAM=protein_domains.txt
 
+mkdir -p $DIR
 cd $DIR
-wget $SITE_INTERACTIONS -O $FILE_INTERACTIONS
-gunzip $FILE_INTERACTIONS
-wget $SITE_PFAM -O $FILE_PFAM
+wget -O $FILE_INTERACTIONS $SITE_INTERACTIONS
+gunzip -f $FILE_INTERACTIONS
+wget -O $FILE_PFAM  $SITE_PFAM
 cd ..
