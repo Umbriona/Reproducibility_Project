401be0ad8085aa3fa753e94ae532e5f31e308048
Umbriona <33811221+Umbriona@users.noreply.github.com>
2021-06-30 00:21:46 +0200

    Download image

--- src/download_data.sh
+++ src/download_data.sh
@@ -1,9 +1,11 @@
 DIR=data/
 SITE_INTERACTIONS=https://stringdb-static.org/download/protein.links.v11.0/9606.protein.links.v11.0.txt.gz
 SITE_PFAM=https://chalmersuniversity.box.com/shared/static/08tmtk9adq43f9af9z7b91i0bnpqhd15.txt
+SITE_IMAGE=https://chalmersuniversity.box.com/shared/static/lfuvsw6v6smjjxa24gv5dj76mvp6pgxs.sif
 
 FILE_INTERACTIONS=protein_links.txt.gz
 FILE_PFAM=protein_domains.txt
+FILE_IMAGE=image.sif
 
 mkdir -p $DIR
 cd $DIR
@@ -11,3 +13,5 @@ wget -O $FILE_INTERACTIONS $SITE_INTERACTIONS
 gunzip -f $FILE_INTERACTIONS
 wget -O $FILE_PFAM  $SITE_PFAM
 cd ..
+wget -O $FILE_IMAGE $FILE_IMAGE
+
